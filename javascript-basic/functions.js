f732c318cd843334fb047419bde6a7389bdc2f94
mengeroshi <dpllgr@hotmail.com>
2021-01-18 22:39:49 -0600

    basic js folder added

---
+++ javascript-basic/functions.js
@@ -0,0 +1,14 @@
+
+
+console.log(declarativa())
+console.log(expresiva())
+
+
+
+const expresiva =  function(){
+    return "Esta es una funcion expresiva";
+}
+
+function declarativa(){
+    return "Esta es una funcion declarativa";
+};
