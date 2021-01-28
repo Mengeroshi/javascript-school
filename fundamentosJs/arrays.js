378e6edc6e1bcf8a247cecf422e3703ed0bfd2de
mengeroshi <dpllgr@hotmail.com>
2021-01-27 23:41:50 -0600

    reduce and filter methods arrays

--- fundamentosJs/arrays.js
+++ fundamentosJs/arrays.js
@@ -1,40 +1,46 @@
 let sacha = {
     nombre: 'Sacha',
     apellido: 'Lifszyc',
-    altura: 1.72
+    altura: 1.72,
+    cantidadDeLibros: 111
 }
 
 let alan = {
     nombre: 'Alan',
     apellido: 'Perez',
-    altura: 1.86
+    altura: 1.86,
+    cantidadDeLibros: 78
 }
 
 let martin = {
     nombre: 'Martin',
     apellido: 'Gomez',
-    altura: 1.85
+    altura: 1.85,
+    cantidadDeLibros: 132
 }
 
 let dario = {
     nombre: 'Dario',
     apellido: 'Juarez',
-    altura: 1.71
+    altura: 1.71,
+    cantidadDeLibros: 90
 }
 
 let vicky = {
     nombre: 'Vicky',
     apellido: 'Zapata',
-    altura: 1.56
+    altura: 1.56,
+    cantidadDeLibros: 91
 }
 
 let paula = {
     nombre: 'Paula',
     apellido: 'Barros',
-    altura: 1.76
+    altura: 1.76,
+    cantidadDeLibros: 182
 }
 
-let personas = [sacha, alan, martin,dario, vicky];
+let personas = [sacha, alan, martin,dario, vicky,paula];
 console.log(personas);
 
 
@@ -47,3 +53,34 @@ let personasAltas = personas.filter(persona => persona.altura > 1.8);
 
 let personasBajas = personas.filter(persona => persona.altura < 1.8);
 console.log(personasBajas);
+
+//mutando el onbjeto original
+
+//let personasCms = personas.map( persona =>{
+//    persona.altura *= 100
+//    return persona;
+//} );
+
+
+//Sin modificacion del objeto
+let personasCms = personas.map( persona =>({
+    ...persona,
+    altura: persona.altura * 100,
+    })
+);
+
+
+
+console.log("modificado",personasCms)
+console.log("original",personas);
+
+
+let totalDeLibros = personas.reduce((acum, {cantidadDeLibros}) => acum + cantidadDeLibros, 0);
+
+//const reducer = (acum, persona)=>{
+//   return acum + persona.cantidadDeLibros;
+//}
+
+//let totalDeLibros = personas.reduce(reducer, 0);
+
+console.log(totalDeLibros)
