b8ea3ad0cbc0f4d2235caddaac73ff6a4d1547a5
mengeroshi <dpllgr@hotmail.com>
2021-02-12 11:37:55 -0600

    asyncronism curse done

---
+++ asyncronismo/src/async/challenge.js
@@ -0,0 +1,23 @@
+const fetchData = require('../utils/fetchData');
+
+let API = 'https://rickandmortyapi.com/api/character/';
+
+const anotherFn  = async  urlApi=>{
+    try{
+        const data = await fetchData (urlApi);
+        const character = await fetchData(`${API}${data.results[0].id}`);
+        const origin = await fetchData(character.origin.url);
+
+        console.log(data.info.count);
+        console.log(character.name);
+        console.log(origin.dimension);
+
+    }catch(error){
+        console.error(error)
+    }
+}
+
+
+console.log('before')
+anotherFn(API);
+console.log('after')
