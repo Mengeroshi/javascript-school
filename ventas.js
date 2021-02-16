adfad25ddea64ef0d4a19d8546b444c61255589d
mengeroshi <dpllgr@hotmail.com>
2021-02-15 22:08:24 -0600

    german database GS added

---
+++ ventas.js
@@ -0,0 +1,106 @@
+const transaccion =  {
+    id:"XXXXXXXX",
+    comprador: "IdComprador",
+    direccion: comprador.direccion,
+    vendedores:[
+        {
+            id: "xxxxx",
+            nombreVendedor: id.nombre,
+            direccion: "calle xxxxx, ssss",
+            guiaLogistica: "DHLXXXXXX",
+            urlPDFGuia: "httpps:/guialink.mx",
+            statusEnvio: guiaLogistica.status, //enviado, no enviado, en camnino, etc
+            articulosVendidos: [
+                {
+                    id: "xxxxxx",
+                    articuloNombre: "memoria ram xxxxx",
+                    precio: articulo.precio,
+                },
+            ],
+            
+        },
+        {
+            id: "xxxxx",
+            nombreVendedor: id.nombre,
+            direccion: "calle xxxxx, ssss",
+            guiaLogistica: "DHLXXXXXX",
+            urlPDFGuia: "httpps:/guialink.mx",
+            statusEnvio: guiaLogistica.status, //enviado, no enviado, en camnino, etc
+            articulosVendidos: [
+                {
+                    id: "xxxxxx",
+                    articuloNombre: "memoria ram xxxxx",
+                    precio: articulo.precio,
+                },
+            ],
+            
+        },
+        {
+            id: "xxxxx",
+            nombreVendedor: id.nombre,
+            direccion: "calle xxxxx, ssss",
+            guiaLogistica: "DHLXXXXXX",
+            urlPDFGuia: "httpps:/guialink.mx",
+            statusEnvio: guiaLogistica.status, //enviado, no enviado, en camnino, etc
+            articulosVendidos: [
+                {
+                    id: "xxxxxx",
+                    articuloNombre: "memoria ram xxxxx",
+                    precio: articulo.precio,
+                },
+            ],
+            
+        }
+    ]
+}
+
+
+
+    compras: [
+        {
+            id: "xxxxxx",
+            articulo: "memoria ram xxxxx",
+            precio: articulo.precio,
+            vendedor: articulo.vendedor,
+            direccionEnvio: vendedor.direccion,
+            guiaLogistica: "DHLXXXXXX",
+            statusEnvio: guiaLogistica.status, //enviado, no enviado, en camnino, etc
+        },
+        {
+            id: "xxxxxx",
+            articulo: "memoria ram xxxxx",
+            precio: articulo.precio,
+            vendedor: articulo.vendedor,
+            direccionEnvio: vendedor.direccion,
+            guiaLogistica: "DHLXXXXXX",
+            statusEnvio: guiaLogistica.status, //enviado, no enviado, en camnino, etc
+        },
+        {
+            id: "xxxxxx",
+            articulo: "memoria ram xxxxx",
+            precio: articulo.precio,
+            vendedor: articulo.vendedor,
+            direccionEnvio: vendedor.direccion,
+            guiaLogistica: "DHLXXXXXX",
+            statusEnvio: guiaLogistica.status, //enviado, no enviado, en camnino, etc
+        },
+    ]
+}
+
+transaccionesPlataforma = [
+
+]
+
+
+
+const ventas = [
+    transaccionesPlataforma[100].compras[1], 
+    transaccionesPlataforma[320].compras[0], 
+    transaccionesPlataforma[500].compras[2],
+]
+
+
+const compras = [
+    transaccionesPlataforma[500].compras,
+    transaccionesPlataforma[600].compras,
+]
